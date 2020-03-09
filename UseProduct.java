e8906d84659054591ab68f58c30babe387ac7240
Susan Driskill <56049351+susandriskill@users.noreply.github.com>
2020-03-09 09:50:55 -0700

    Adds UseProduct.Java

--- UseProduct.java
+++ UseProduct.java
@@ -8,12 +8,15 @@
 public class UseProduct {
 
 	public static void main(String[] args) {
+		//Tests
+		//Create 2 - 3 different products and call the toString method to
+		//display their data
 		Product p1 = new Product(1001, "Gold Star Necklace", 38.00, 30);
-		System.out.println(p1.toString() + p1.getPriceFormatted());
+		System.out.println(p1.toString());
 		Product p2 = new Product(1002, "Silver Moon Earring Set", 22.00, 48);
-		System.out.println(p2.toString() + p2.getPriceFormatted());
+		System.out.println(p2.toString());
 		Product p3 = new Product(1003, "Tungsten Plain Ring Band", 80.00, 100);
-		System.out.println(p3.toString() + p3.getPriceFormatted());
+		System.out.println(p3.toString());
 
 	}
 
